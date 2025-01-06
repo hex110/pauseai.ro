26814e915d3dc6131f7ec0475bf5900808fe359f
hex110 <robertstroiofficial@gmail.com>
2025-01-06 02:06:21 +0200

    Fixed favicon

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -13,6 +13,12 @@ export default function App({ Component }: AppProps) {
           content="Împreună pentru un viitor sigur cu AI"
         />
         <link rel="stylesheet" href="/styles.css" />
+        
+        <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg"/>
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
       </head>
       <body>
         <Component />
